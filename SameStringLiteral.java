71cd48f78df1175b299e2b92c80819bf8278170d
zhaoziwen <516160271@qq.com>
2016-08-12 17:31:22 +0800

    jvm byte code

---
+++ SameStringLiteral.java
@@ -0,0 +1,9 @@
+/**
+ * Created by infosea on 2016/3/7.
+ */
+public class SameStringLiteral {
+    public static void main(String[] args){
+        String abc1_ref=new String("abc");
+        String abc_ref ="abc";
+    }
+}
